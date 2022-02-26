3c0a6a86c370c5e6cf0515665e95d0dba13d7f68
Nat Samson <natsamson1@gmail.com>
2022-02-26 18:23:47 +0000

    Implement Add, including tests

--- src/sml/instructions/AddInstruction.java
+++ src/sml/instructions/AddInstruction.java
@@ -6,16 +6,49 @@ import sml.Machine;
 /**
  * This class represents the Add instruction from the language.
  *
- * @author ...
+ * @author nsamso01
  */
 public class AddInstruction extends Instruction {
 
+    private final int opTarget;
+    private final int op1;
+    private final int op2;
+
+    /**
+     * Constructor: Initialises a new Add instruction.
+     *
+     * @param label of the instruction
+     * @param register where the result of the calculation is stored
+     * @param s1 first operand
+     * @param s2 second operand
+     */
     public AddInstruction(String label, int register, int s1, int s2){
-        super("Something", "Something Else");
+        super(label, "add");
+        this.opTarget = register;
+        this.op1 = s1;
+        this.op2 = s2;
     }
 
+    /**
+     * Executes this instruction using the specified machine.
+     *
+     * @param m machine processing the instructions
+     */
     @Override
     public void execute(Machine m) {
-        // TODO: Implement
+        int val1 = m.getRegisters().getRegister(op1);
+        int val2 = m.getRegisters().getRegister(op2);
+
+        m.getRegisters().setRegister(opTarget, val1 + val2);
+    }
+
+    /**
+     * String representation of the Add instruction, including its fields.
+     *
+     * @return String in this form: "Label: add. Store result of rX + rY in rZ"
+     */
+    @Override
+    public String toString() {
+        return super.toString() + ". Store result of r" + op1 + " + r" + op2 + " in r" + opTarget;
     }
 }
